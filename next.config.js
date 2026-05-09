ca874950908a76da725badc468ef9864be8af522
Jarryd <jarryd@roamly.biz>
2026-05-09 22:00:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+module.exports = nextConfig
